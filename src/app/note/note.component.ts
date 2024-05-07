082d674ec03e07cc096b3678c2731e00e91dcafd
GeorgiHr <gehr1235@gmail.com>
2024-05-07 15:39:04 +0300

    Added all functions

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './note.component.html',
   styleUrl: './note.component.scss'
 })
 export class NoteComponent {
+  public isNoteSelected = false;
+  public index = 0;
+
+  modelTitle = '';
+  modelContent = '';
   
+  modelTitleSize = 5;
+  modelContentSize = 7;
+
+  public noteCollection = [
+    {
+      title: 'asdasd', content: 'asdasd'
+    }
+  ]
+
+  public addNote(){
+    let note = {
+      title: this.modelTitle,
+      content: this.modelContent
+    }
+
+    if(this.isNoteSelected){
+      this.noteCollection[this.index].title = this.modelTitle;
+      this.noteCollection[this.index].content = this.modelContent;
+      this.isNoteSelected = false;
+    }else{
+      this.noteCollection.push(note);
+    }
+
+  }
+  
+  public selectNote(index){
+    this.index = index;
+    console.log(index);
+    this.isNoteSelected = true;
+  }
+
+  public deleteNote() {
+    this.noteCollection.splice(this.index, 1);
+    this.resetValues();
+  }
+
+  public editNote() {
+    this.modelTitle = this.noteCollection[this.index].title;
+    this.modelContent = this.noteCollection[this.index].content;
+
+  }
+
+  private resetValues() {
+    this.isNoteSelected = false;
+    this.modelTitle = '';
+    this.modelContent = '';
+  }
 }
